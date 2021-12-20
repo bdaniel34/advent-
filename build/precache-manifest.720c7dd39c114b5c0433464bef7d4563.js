29a76e381d05e0a2d0dafdf438d290b650d9f9fb
unknown <deandon@hotmail.es>
2021-12-20 12:54:47 +0100

    first commit

---
+++ build/precache-manifest.720c7dd39c114b5c0433464bef7d4563.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e157d2ceeadd3ca3794e7d6f8bae9835",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40b64a6137f19c264d1b",
+    "url": "/static/js/2.a18134f5.chunk.js"
+  },
+  {
+    "revision": "050956d0f2a5eff0db7b",
+    "url": "/static/js/main.40f1ef65.chunk.js"
+  },
+  {
+    "revision": "db927b1f5992b42704f8",
+    "url": "/static/js/runtime-main.2afe415d.js"
+  },
+  {
+    "revision": "fe63257737c2cbb1bd873201c675fb40",
+    "url": "/static/media/hatch_backdrop.fe632577.jpg"
+  }
+]);
